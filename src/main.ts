525ce578b88f668e7de64950511657461720900b
ruohl <rudolfgarciabohl@gmail.com>
2026-02-10 20:38:46 -0300

    feat: añado configuración del Pipe de validación

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,17 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const port = process.env.PORT ?? 3000;
   const app = await NestFactory.create(AppModule);
+  
+  app.useGlobalPipes(new ValidationPipe({
+    whitelist: true,
+    forbidNonWhitelisted: true,
+    transform: true,
+  }));
+  
   app.setGlobalPrefix('/v1/api')
   await app.listen(port);
 
